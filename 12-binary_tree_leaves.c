46fa98e376d0429c2062f96e1e5e674120261f13
ahmed-my <yahaya.mymohammed@gmail.com>
2024-01-03 17:02:50 +0000

    binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,12 +8,12 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t tree_leaves = 0;
+	size_t leaves = 0;
 
 	if (tree)
 	{
-		tree_leaves += (!tree->left && !tree->right) ? 1 : 0;
-		tree_leaves += binary_tree_leaves(tree->left);
-		tree_leaves += binary_tree_leaves(tree->right);
+		leaves += (!tree->left && !tree->right) ? 1 : 0;
+		leaves += binary_tree_leaves(tree->left);
+		leaves += binary_tree_leaves(tree->right);
 	}
-	return (tree_leaves);
+	return (leaves);
