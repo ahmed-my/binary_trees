492f7a7107d69be765f3762e61f0f0009a2e0348
ahmed-my <yahaya.mymohammed@gmail.com>
2024-01-04 19:25:47 +0000

    binary tree

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -12,11 +12,8 @@ heap_t *array_to_heap(int *array, size_t size)
 	unsigned int j;
 	heap_t *root = NULL;
 
-while (j < size)
-{
-heap_insert(&root, array[j]);
-j++;
-}
+	for (j = 0; j < size; j++)
+		heap_insert(&root, array[j]);
 
 	return (root);
 }
